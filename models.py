094719932c9f75277f1fb95a192eef7c1d17f83b
AndreiRO <andysstef@yahoo.com>
2015-04-09 13:26:52 +0300

    Base test for sqlalchemy

---
+++ models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///testdb.db', echo=True)
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Person(Base):
+    __tablename__ = 'people'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    job = Column(String)
+
+    def repr(self):
+        return "<Person(name = '%s', job = '%s')>" % (self.name, self.job)
+
+
+print Person.__table__
+
+Base.metadata.create_all(engine)
+
+Messi = Person(name='Messi', job='Football player')
+Mutu = Person(name='Mutu', job='Football player')
+Marica = Person(name='Marica', job='Football player')
+Torje = Person(name='Torje', job='Football player')
+Hagi = Person(name='Hagi', job='Football player')
+Goian = Person(name='Goian', job='Football player')
+
+session.add_all([Messi, Mutu, Marica, Torje, Hagi, Goian])
+session.commit()
+
+
